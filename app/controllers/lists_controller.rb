991780e4a314ecf82117933922411f5d11d730bb
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-07-13 20:14:07 +0900

    change platform on gemfile.lock

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,9 +18,15 @@ class ListsController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image_url)
   end
 end
