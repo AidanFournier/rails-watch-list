162189b5aff70b9281d14feebaf6c382bee0c423
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-07-15 21:56:41 +0900

    Options dropdown - delete added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require 'open-uri'
 
-if Rails.env.development?
-  puts "Destroying all movies..."
-  Movie.destroy_all
-end
+
+puts "Destroying all movies..."
+Movie.destroy_all
+
 
 # puts "New movies in production... 🎬"
 # Movie.create!(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
